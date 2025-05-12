d07bf503be5c4fae13ecee36f55e37914c1cd6ce
kodjai <67613532+kodjai@users.noreply.github.com>
2025-05-12 09:30:24 +0900

    Add files via upload

---
+++ wCapture/cap.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace wCapture
+{
+    partial class cap
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.displayControl = new PvGUIDotNet.PvDisplayControl();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnConfigure = new System.Windows.Forms.Button();
+            this.btnStartstream = new System.Windows.Forms.Button();
+            this.btnStopstream = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.displayControl1 = new PvGUIDotNet.PvDisplayControl();
+            this.btnConnect1 = new System.Windows.Forms.Button();
+            this.btnStartstream1 = new System.Windows.Forms.Button();
+            this.btnStopstream1 = new System.Windows.Forms.Button();
+            this.btnConfigure1 = new System.Windows.Forms.Button();
+            this.btnDisconnect1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayControl
+            // 
+            this.displayControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.displayControl.BackColor = System.Drawing.Color.Transparent;
+            this.displayControl.BackgroundColor = System.Drawing.Color.DarkGray;
+            this.displayControl.Location = new System.Drawing.Point(96, 12);
+            this.displayControl.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.displayControl.Name = "displayControl";
+            this.displayControl.ROI = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.displayControl.Size = new System.Drawing.Size(331, 307);
+            this.displayControl.TabIndex = 8;
+            this.displayControl.TextOverlay = "";
+            this.displayControl.TextOverlayColor = System.Drawing.Color.Lime;
+            this.displayControl.TextOverlayOffsetX = 10;
+            this.displayControl.TextOverlayOffsetY = 10;
+            this.displayControl.TextOverlaySize = 18;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(30, 11);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(60, 33);
+            this.btnConnect.TabIndex = 11;
+            this.btnConnect.Text = "connect";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnConfigure
+            // 
+            this.btnConfigure.Enabled = false;
+            this.btnConfigure.Location = new System.Drawing.Point(30, 70);
+            this.btnConfigure.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnConfigure.Name = "btnConfigure";
+            this.btnConfigure.Size = new System.Drawing.Size(60, 38);
+            this.btnConfigure.TabIndex = 10;
+            this.btnConfigure.Text = "configure";
+            this.btnConfigure.UseVisualStyleBackColor = true;
+            this.btnConfigure.Click += new System.EventHandler(this.btnConfigure_Click);
+            // 
+            // btnStartstream
+            // 
+            this.btnStartstream.Enabled = false;
+            this.btnStartstream.Location = new System.Drawing.Point(30, 131);
+            this.btnStartstream.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnStartstream.Name = "btnStartstream";
+            this.btnStartstream.Size = new System.Drawing.Size(60, 34);
+            this.btnStartstream.TabIndex = 12;
+            this.btnStartstream.Text = "startstream";
+            this.btnStartstream.UseVisualStyleBackColor = true;
+            this.btnStartstream.Click += new System.EventHandler(this.btnStartstream_Click);
+            // 
+            // btnStopstream
+            // 
+            this.btnStopstream.Enabled = false;
+            this.btnStopstream.Location = new System.Drawing.Point(30, 197);
+            this.btnStopstream.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnStopstream.Name = "btnStopstream";
+            this.btnStopstream.Size = new System.Drawing.Size(60, 37);
+            this.btnStopstream.TabIndex = 13;
+            this.btnStopstream.Text = "stopstream";
+            this.btnStopstream.UseVisualStyleBackColor = true;
+            this.btnStopstream.Click += new System.EventHandler(this.btnStopstream_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(30, 262);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(60, 32);
+            this.btnDisconnect.TabIndex = 14;
+            this.btnDisconnect.Text = "disconnetct";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Interval = 1000;
+            this.refreshTimer.Tick += new System.EventHandler(this.refresh_timer_Tick);
+            // 
+            // displayControl1
+            // 
+            this.displayControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.displayControl1.BackColor = System.Drawing.Color.Transparent;
+            this.displayControl1.BackgroundColor = System.Drawing.Color.DarkGray;
+            this.displayControl1.Location = new System.Drawing.Point(513, 12);
+            this.displayControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.displayControl1.Name = "displayControl1";
+            this.displayControl1.ROI = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.displayControl1.Size = new System.Drawing.Size(331, 307);
+            this.displayControl1.TabIndex = 15;
+            this.displayControl1.TextOverlay = "";
+            this.displayControl1.TextOverlayColor = System.Drawing.Color.Lime;
+            this.displayControl1.TextOverlayOffsetX = 10;
+            this.displayControl1.TextOverlayOffsetY = 10;
+            this.displayControl1.TextOverlaySize = 18;
+            // 
+            // btnConnect1
+            // 
+            this.btnConnect1.Location = new System.Drawing.Point(448, 11);
+            this.btnConnect1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnConnect1.Name = "btnConnect1";
+            this.btnConnect1.Size = new System.Drawing.Size(59, 33);
+            this.btnConnect1.TabIndex = 16;
+            this.btnConnect1.Text = "connect";
+            this.btnConnect1.Click += new System.EventHandler(this.btnConnect1_Click);
+            // 
+            // btnStartstream1
+            // 
+            this.btnStartstream1.Enabled = false;
+            this.btnStartstream1.Location = new System.Drawing.Point(448, 131);
+            this.btnStartstream1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnStartstream1.Name = "btnStartstream1";
+            this.btnStartstream1.Size = new System.Drawing.Size(59, 34);
+            this.btnStartstream1.TabIndex = 18;
+            this.btnStartstream1.Text = "startstream";
+            this.btnStartstream1.UseVisualStyleBackColor = true;
+            this.btnStartstream1.Click += new System.EventHandler(this.btnStartstream1_Click);
+            // 
+            // btnStopstream1
+            // 
+            this.btnStopstream1.Enabled = false;
+            this.btnStopstream1.Location = new System.Drawing.Point(448, 197);
+            this.btnStopstream1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnStopstream1.Name = "btnStopstream1";
+            this.btnStopstream1.Size = new System.Drawing.Size(59, 37);
+            this.btnStopstream1.TabIndex = 19;
+            this.btnStopstream1.Text = "stopstream";
+            this.btnStopstream1.UseVisualStyleBackColor = true;
+            this.btnStopstream1.Click += new System.EventHandler(this.btnStopstream1_Click);
+            // 
+            // btnConfigure1
+            // 
+            this.btnConfigure1.Enabled = false;
+            this.btnConfigure1.Location = new System.Drawing.Point(448, 70);
+            this.btnConfigure1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnConfigure1.Name = "btnConfigure1";
+            this.btnConfigure1.Size = new System.Drawing.Size(59, 38);
+            this.btnConfigure1.TabIndex = 20;
+            this.btnConfigure1.Text = "configure";
+            this.btnConfigure1.UseVisualStyleBackColor = true;
+            this.btnConfigure1.Click += new System.EventHandler(this.btnConfigure1_Click);
+            // 
+            // btnDisconnect1
+            // 
+            this.btnDisconnect1.Enabled = false;
+            this.btnDisconnect1.Location = new System.Drawing.Point(448, 262);
+            this.btnDisconnect1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnDisconnect1.Name = "btnDisconnect1";
+            this.btnDisconnect1.Size = new System.Drawing.Size(59, 32);
+            this.btnDisconnect1.TabIndex = 21;
+            this.btnDisconnect1.Text = "disconnetct";
+            this.btnDisconnect1.UseVisualStyleBackColor = true;
+            this.btnDisconnect1.Click += new System.EventHandler(this.btnDisconnect1_Click);
+            // 
+            // cap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 366);
+            this.Controls.Add(this.btnDisconnect1);
+            this.Controls.Add(this.btnConfigure1);
+            this.Controls.Add(this.btnStopstream1);
+            this.Controls.Add(this.btnStartstream1);
+            this.Controls.Add(this.btnConnect1);
+            this.Controls.Add(this.displayControl1);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnStopstream);
+            this.Controls.Add(this.btnStartstream);
+            this.Controls.Add(this.btnConfigure);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.displayControl);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "cap";
+            this.Text = "Cap";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PvGUIDotNet.PvDisplayControl displayControl;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnConfigure;
+        private System.Windows.Forms.Button btnStartstream;
+        private System.Windows.Forms.Button btnStopstream;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Timer refreshTimer;
+        private PvGUIDotNet.PvDisplayControl displayControl1;
+        private System.Windows.Forms.Button btnConnect1;
+        private System.Windows.Forms.Button btnStartstream1;
+        private System.Windows.Forms.Button btnStopstream1;
+        private System.Windows.Forms.Button btnConfigure1;
+        private System.Windows.Forms.Button btnDisconnect1;
+    }
+}
+
---
+++ wCapture/cap.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PvDotNet;
+using PvGUIDotNet;
+
+namespace wCapture
+{
+    public partial class cap : Form
+    {
+        public cap()
+        {
+            InitializeComponent();
+        }
+
+        private const UInt16 cBufferCount = 16;
+
+        private PvDevice mDevice0 = null;            
+        private PvDevice mDevice1 = null;           
+
+
+        private PvStream mStream0 = null;
+        private PvStream mStream1 = null;
+
+        private PvPipeline mPipeline0 = null;
+        private PvPipeline mPipeline1 = null;
+
+        private Thread mThread0 = null;
+        private Thread mThread1 = null;
+
+        private bool mIsStopping0 = false;
+        private bool mIsStopping1 = false;
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            PvDeviceFinderForm ConnectForm = new PvDeviceFinderForm();          
+            if ((ConnectForm.ShowDialog() == DialogResult.OK) && (ConnectForm.Selected != null))
+            {
+                try
+                {
+                    PvDeviceInfo lDeviceInfo = ConnectForm.Selected as PvDeviceInfo;            
+
+                    // Create and connect device.
+                    mDevice0 = PvDevice.CreateAndConnect(lDeviceInfo);                           
+
+                    // Create and Open stream.
+                    mStream0 = PvStream.CreateAndOpen(lDeviceInfo);                              
+
+                    // Create pipeline.
+                    mPipeline0 = new PvPipeline(mStream0);                                        
+                }
+                catch (PvException ex)
+                {
+
+                    MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Close();
+                }
+            }
+            
+            btnConfigure.Enabled = true;
+        }
+
+
+        private void btnConfigure_Click(object sender, EventArgs e)
+        {
+            btnConfigure.Enabled = false;
+            try
+            {
+                // Perform GigE Vision only configuration
+                PvDeviceGEV lDGEV = mDevice0 as PvDeviceGEV;
+                if (lDGEV != null)
+                {
+                    // Negotiate packet size    
+                    lDGEV.NegotiatePacketSize();
+
+                    // Set stream destination.  
+                    PvStreamGEV lSGEV = mStream0 as PvStreamGEV;
+                    lDGEV.SetStreamDestination(lSGEV.LocalIPAddress, lSGEV.LocalPort);
+                }
+
+                // Perform GigE Vision only configuration  
+                // Read payload size, set buffer size the pipeline will use to allocate buffers. 
+                mPipeline0.BufferSize = mDevice0.PayloadSize;
+
+                // Set buffer count. Use more buffers (at expense of using more memory) to eliminate missing block IDs.
+                mPipeline0.BufferCount = cBufferCount;
+
+            }
+            catch (PvException ex)
+            {
+                MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Close();
+            }
+            btnStartstream.Enabled = true;
+        }
+
+
+        private void btnStartstream_Click(object sender, EventArgs e)
+        {
+            btnStartstream.Enabled = false;
+            mIsStopping0 = false;
+            //mDevice0.Parameters.SetIntegerValue("TLParamsLocked", 1);  
+
+            mPipeline0.Start();
+
+            // Start display thread.                                
+            mThread0 = new Thread(new ParameterizedThreadStart(ThreadProc));
+            cap lP1 = this;
+            object[] lParameters = new object[] { lP1 };
+         
+            mThread0.Start(lParameters);
+
+            // Enables streaming before sending the AcquisitionStart command.
+            mDevice0.StreamEnable();
+
+            // Start acquisition on the device.
+            mDevice0.Parameters.ExecuteCommand("AcquisitionStart");
+
+            //refreshTimer.Start();
+            btnStopstream.Enabled = true;
+        }
+
+        private static void ThreadProc(object aParameters)
+        {
+            object[] lParameters = (object[])aParameters;
+            cap lThis = (cap)lParameters[0];
+
+            PvBuffer lBuffer = null;
+
+
+
+            for (; ;)
+            {
+
+                if (lThis.mIsStopping0)
+                {
+                    // Signaled to terminate thread, return.
+                    return;
+                }
+                // Retrieve next buffer from acquisition pipeline.
+                PvResult lResult = lThis.mPipeline0.RetrieveNextBuffer(ref lBuffer);
+                if (lResult.IsOK)
+                {
+                    // Operation result of buffer is OK, display.
+                    if (lBuffer.OperationResult.IsOK)
+                    {
+                        lThis.displayControl.Display(lBuffer);
+                    }
+
+                    // We got a buffer (good or not) we must release it back.
+                    lThis.mPipeline0.ReleaseBuffer(lBuffer);
+                }
+            }
+        }
+
+        private static void ThreadProc1(object aParameters)
+        {
+            object[] lParameters = (object[])aParameters;
+            cap lThis = (cap)lParameters[0];
+
+            PvBuffer lBuffer = null;
+
+
+
+            for (; ; )
+            {
+
+                if (lThis.mIsStopping1)
+                {
+                    // Signaled to terminate thread, return.
+                    return;
+                }
+                // Retrieve next buffer from acquisition pipeline.
+                PvResult lResult = lThis.mPipeline1.RetrieveNextBuffer(ref lBuffer);
+                if (lResult.IsOK)
+                {
+                    // Operation result of buffer is OK, display.
+                    if (lBuffer.OperationResult.IsOK)
+                    {
+                        lThis.displayControl1.Display(lBuffer);
+                    }
+
+                    // We got a buffer (good or not) we must release it back.
+                    lThis.mPipeline1.ReleaseBuffer(lBuffer);
+                }
+            }
+        }
+
+        private void btnStopstream_Click(object sender, EventArgs e)
+        {
+            //mDevice0.Parameters.SetIntegerValue("TLParamsLocked", 0);
+            //refreshTimer.Stop();
+            btnStopstream.Enabled = false;
+            mIsStopping0 = false;
+            // Stop acquisition. 
+            mDevice0.Parameters.ExecuteCommand("AcquisitionStop"); //ExecuteCommand("AcquisitionStop");
+            // Disable streaming after sending the AcquisitionStop command. 
+            mDevice0.StreamDisable();
+
+            // Stop the pipeline.
+            mPipeline0.Stop();
+
+            // Stop display thread.
+            mIsStopping0 = true;
+            mThread0.Join();
+            mThread0 = null;
+            btnDisconnect.Enabled = true;
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            btnDisconnect.Enabled = false;
+            if (mStream0 != null)
+            {
+                // Close and release stream
+                mStream0.Close();
+                mStream0 = null;
+            }
+
+            if (mDevice0 != null)
+            {
+                // Disconnect and release device
+                mDevice0.Disconnect();
+                mDevice0 = null;
+            }
+            btnConnect.Enabled = true;
+        }
+
+        private void refresh_timer_Tick(object sender, EventArgs e)
+        {
+            PvGenParameterArray lStreamParams = mStream0.Parameters;
+            PvGenParameterArray lStreamParams1 = mStream1.Parameters;
+            lStreamParams.InvalidateCache();
+            lStreamParams1.InvalidateCache();
+        }
+
+        private void btnConnect1_Click(object sender, EventArgs e)
+        {
+            btnConnect1.Enabled = false;
+            PvDeviceFinderForm ConnectForm = new PvDeviceFinderForm();          
+            if ((ConnectForm.ShowDialog() == DialogResult.OK) && (ConnectForm.Selected != null))
+            {
+                try
+                {
+                     //获得所选设备参数
+                    PvDeviceInfo lDeviceInfo1 = ConnectForm.Selected as PvDeviceInfo;
+
+                    // Create and connect device.                         
+                    mDevice1 = PvDevice.CreateAndConnect(lDeviceInfo1);
+
+                    // Create and Open stream.                        
+                    mStream1 = PvStream.CreateAndOpen(lDeviceInfo1);
+
+                    // Create pipeline.                               
+                    mPipeline1 = new PvPipeline(mStream1);
+                }
+                catch (PvException ex)
+                {
+
+                    MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Close();
+                }
+            }
+
+            btnConfigure1.Enabled = true;
+        }
+
+
+
+        private void btnConfigure1_Click(object sender, EventArgs e)
+        {
+            btnConfigure1.Enabled = false;
+            try
+            {
+
+                // Perform GigE Vision only configuration   
+                PvDeviceGEV lDGEV1 = mDevice1 as PvDeviceGEV;
+                if (lDGEV1 != null)
+                {
+                    // Negotiate packet size    
+                    lDGEV1.NegotiatePacketSize();
+
+                    // Set stream destination.  
+                    PvStreamGEV lSGEV = mStream1 as PvStreamGEV;
+                    lDGEV1.SetStreamDestination(lSGEV.LocalIPAddress, lSGEV.LocalPort);
+                }
+
+                // Read payload size, set buffer size the pipeline will use to allocate buffers. 
+
+                mPipeline1.BufferSize = mDevice1.PayloadSize;
+
+                // Set buffer count. Use more buffers (at expense of using more memory) to eliminate missing block IDs.
+
+                mPipeline1.BufferCount = cBufferCount;
+            }
+            catch (PvException ex)
+            {
+                MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Close();
+            }
+            btnStartstream1.Enabled = true;
+        }
+
+        private void btnStartstream1_Click(object sender, EventArgs e)
+        {
+            btnStartstream1.Enabled = false;
+            mIsStopping1 = false;
+            //mDevice0.Parameters.SetIntegerValue("TLParamsLocked", 1);  
+
+            mPipeline1.Start();
+
+            // Start display thread.                                
+            mThread1 = new Thread(new ParameterizedThreadStart(ThreadProc1));
+            cap lP1 = this;
+            object[] lParameters = new object[] { lP1 };
+
+            mThread1.Start(lParameters);
+
+            // Enables streaming before sending the AcquisitionStart command.
+            mDevice1.StreamEnable();
+
+            // Start acquisition on the device.
+            mDevice1.Parameters.ExecuteCommand("AcquisitionStart");
+
+            //refreshTimer.Start();
+            btnStopstream1.Enabled = true;
+        }
+
+        private void btnStopstream1_Click(object sender, EventArgs e)
+        {
+            //mDevice0.Parameters.SetIntegerValue("TLParamsLocked", 0);
+            //refreshTimer.Stop();
+            btnStopstream1.Enabled = false;
+            mIsStopping1 = false;
+            // Stop acquisition. 
+             //ExecuteCommand("AcquisitionStop");
+            mDevice1.Parameters.ExecuteCommand("AcquisitionStop");
+            // Disable streaming after sending the AcquisitionStop command. 
+            mDevice1.StreamDisable();
+
+            // Stop the pipeline.
+            mPipeline1.Stop();
+
+            // Stop display thread.
+            mIsStopping1 = true;
+            mThread1.Join();
+            mThread1 = null;
+            btnDisconnect1.Enabled = true;
+        }
+
+        private void btnDisconnect1_Click(object sender, EventArgs e)
+        {
+            btnDisconnect1.Enabled = false;
+            if (mStream1 != null)
+            {
+                // Close and release stream
+                mStream1.Close();
+                mStream1 = null;
+            }
+
+            if (mDevice1 != null)
+            {
+                // Disconnect and release device
+                mDevice1.Disconnect();
+                mDevice1 = null;
+            }
+            btnConnect1.Enabled = true;
+        }
+
+
+    }
+}
